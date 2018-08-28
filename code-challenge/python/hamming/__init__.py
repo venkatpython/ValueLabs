967bf5e50ceb746eadbdc6d3e4ca43899f4b694d
Venkat K <kvenkatesh.python@gmail.com>
2018-08-28 15:46:18 +0530

    Update __init__.py

--- code-challenge/python/hamming/__init__.py
+++ code-challenge/python/hamming/__init__.py
@@ -11,15 +11,9 @@ def compute(dna1, dna2):
     """
     This function will calculates the hamming distance between the two DNA strands.
 
-    Input: Two DNA strands
+    Input: DNA Strands dna1 and dna2
     :return: Hamming distance between the two DNA strands
     """
     if len(dna1) != len(dna2):
         raise ValueError("Both DNA strands should be equal length.")
     return len([a for a,b in zip(dna1,dna2) if a != b])
-
-
-if __name__ == "__main__":
-    str1 = "GAGCCTACTAACGGGAT"
-    str2 = "CATCGTAATGACGGCCT"
-    print compute(str1, str2)
